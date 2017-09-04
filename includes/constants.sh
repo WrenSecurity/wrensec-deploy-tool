e95f53621b8c7d8458780d9312071476df6b807e
Kortanul <kortanul@protonmail.com>
2017-09-03 23:12:47 -0400

    adds audit, DS SDK, and DS to the project list

--- includes/constants.sh
+++ includes/constants.sh
@@ -2,14 +2,17 @@
 # Shared Constants
 ################################################################################
 export PROJECTS=( \
-  wrensec-parent \
-  wrensec-build-tools \
-  wrensec-bom \
-  wrensec-util \
-  wrensec-i18n-framework \
-  wrensec-guice \
-  wrensec-http-framework \
-  wrensec-rest \
+  "wrensec-parent" \
+  "wrensec-build-tools" \
+  "wrensec-bom" \
+  "wrensec-util" \
+  "wrensec-i18n-framework" \
+  "wrensec-guice" \
+  "wrensec-http-framework" \
+  "wrensec-rest" \
+  "wrensec-audit" \
+  "wrends-sdk" \
+  "wrends" \
 )
 
 export WRENDEPLOY_RC=".wren-deploy.rc"
